ab5b69050ae7f8f3c118817741999a1cd2fe5b41
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-02 14:57:13 -0800

    Add project files.

---
+++ Deck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
+                    Cards.Add(card);
+                }
+            }
+        }
+        public List<Card> Cards { get; set; }
+        public void Shuffle(int times =1 )
+        {
+            
+            for (int i = 0; i < times; i++)
+            {
+          
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, this.Cards.Count);
+                    TempList.Add(this.Cards[randomIndex]);
+                    this.Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = TempList;
+            }
+        }
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i< times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
+        
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+   public class Player
+    {
+        public Player(string name, int beginningBalance)
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+            Name = name;
+    
+        }
+
+        public List<Card> Hand { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public bool isActivelyPlaying  { get; set; }
+        public bool Stay { get; set; }
+
+        public bool Bet(int amount)
+        {
+            if (Balance - amount < 0)
+            {
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+        }
+        
+         
+        // Add Player 
+        public static Game operator+ (Game game, Player player)   
+        {
+            game.Players.Add(player);
+            return game;
+        }
+
+        // Remove Player
+        public static Game operator- (Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
+    }
+}
